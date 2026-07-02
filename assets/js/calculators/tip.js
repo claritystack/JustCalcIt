0163092e563caf8c7cea07c41e39ad907d07a954
claritystack <bagalatharva348@gmail.com>
2026-07-02 13:56:57 +0530

    Update tip.js

--- assets/js/calculators/tip.js
+++ assets/js/calculators/tip.js
@@ -59,14 +59,4 @@ document.getElementById('resetBtn').addEventListener('click', () => {
   calc();
 });
 
-// FAQ accordion
-document.querySelectorAll('.faq-q').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const item   = btn.parentElement;
-    const isOpen = item.classList.contains('open');
-    document.querySelectorAll('.faq-item').forEach(i => i.classList.remove('open'));
-    if (!isOpen) item.classList.add('open');
-  });
-});
-
 calc();
